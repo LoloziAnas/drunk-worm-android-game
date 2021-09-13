60e5a26404ca249e2a202b86f2dece099f6c40c0
LoloziAnas <lolozianas1@gmail.com>
2021-09-13 18:10:38 +0100

    first commit & designin Main Menu

---
+++ app/src/main/java/com/dunkworm21game/dunkworm/MainActivity.java
@@ -0,0 +1,19 @@
+package com.dunkworm21game.dunkworm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View decorView = getWindow().getDecorView();
+
+    }
+}
